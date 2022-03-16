52035a161441ea949a8b16c8b16b2c4e70fc843d
Bkellz28 <ben.kelley28@gmail.com>
2022-03-15 18:31:33 -0700

    began rdt2.2 implementation RDT2_protocol

---
+++ Phase3/RDT2_protocol.py
@@ -0,0 +1,75 @@
+# import socket library
+from socket import *
+import time
+
+
+# updated RDT protocol to rdt2.3
+class RDT2:
+    def __init__(self, hostName, portNum, debugToggle):
+        self.host = hostName
+        self.port = portNum
+        # open socket for use
+        self.UDPsocket = socket(AF_INET, SOCK_DGRAM)
+        # keep track of debugToggle (1 for debug messages, 0 for none)
+        self.debug = debugToggle
+    
+    # data file send
+    def rdt_send(self, sendData, receiver):
+        db = self.debug # grab debug val
+        # calc num packets, data length / 1024 bytes
+        length = len(sendData)
+        numPack = length // int(1024)
+        # add extra packet for any leftover bytes
+        if (length % 1024 != 0): numPack += 1
+        # send numPack to receiver to start the data transfer
+        self.UDPsocket.sendto(str(numPack).encode(), receiver)
+        if (db == 1): print(str(numPack) + ' packets to send...')
+        # create list of packets to send
+        sendList = []
+        for i in range(numPack):
+            packStart = i * 1024 # packet start index
+            packEnd = (i+1) * 1024 # packet end index
+            packet = sendData[packStart:packEnd] # parse packet
+            sendList.append(packet) # add packet to list
+        # check that sendList is same size as numPack
+        if (len(sendList) != numPack):
+            print('ERROR: packet list does not match estimated packet size')
+            print('Ending send call...')
+            return
+        elif (len(sendList) != numPack && db == 1): 
+            print('All packets successfully parsed...')
+        # send packet one at a time and wait for ACK response from server
+        iD = 0 # init identifier as 0, will flip b/t 0 and 1
+        for i in range(len(sendList)):
+            # grab packet and append checksum and identifier
+            msg = sendList[i] # grab packet
+            cs = checksum(msg) # calculate checksum
+            # Corruption needs to be implemented after this line
+            if (iD == 0): iD = 1
+            elif (iD == 1): iD = 0
+            # MORE TO ADD
+        
+    # data file receive
+    def rdt_recv(self):
+        db = self.debug # grab debug val
+        # recv initial message that gives number of packets
+        recvStart, serverAddress = self.UDPsocket.recvfrom(1024)
+        numPack = int(recvStart.decode())
+        if (db == 1): print(str(numPack) + ' packets incoming...')
+        numRecv = 0
+        # receive data packets and ACKnowledge reception from sender
+        while (recvNum != numPack):
+            packet, svrAddr = self.UDPsocket.recvfrom(1028)
+            # parse packet down into message, checksum, and identifier
+            # first byte is identifier, next three is checksum, remainder is message
+
+    
+# checksum for use in send and recv
+def checksum(data):
+    # convert byte data to bits
+    dataBits = bin(int(data.hex(), 16))
+    # initialize checkSum and iterate through bits
+    checkSum = 0
+    for i in range(2, len(dataBits)): # start at 2 since string will start with 0b
+        if dataBits[i] == '1': checkSum += 1
+    return checkSum
