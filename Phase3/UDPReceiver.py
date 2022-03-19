2f29461641c48f613fdded2731e31ff1a3efb39c
Bkellz28 <ben.kelley28@gmail.com>
2022-03-19 12:08:30 -0700

    added sender and receiver to phase3

---
+++ Phase3/UDPReceiver.py
@@ -0,0 +1,30 @@
+# import entire socket & rdt library for use
+from socket import *
+from RDT2_protocol import RDT2
+import numpy as np
+import cv2 as cv
+
+# initialize server info
+rcvrName = '127.0.0.1'
+rcvrPort = 10421
+# create receiver primed for reliable data transfer and bind to local port
+Receiver = RDT2(rcvrName, rcvrPort)
+# leaving first param blank allows for svr to rcv from other hosts
+Receiver.UDPsocket.bind(('', rcvrPort))
+
+# alert user that server is ready
+print("The receiver is ready")
+
+# server reads and modifies message
+# loop receive action for server
+while True:
+    # receive data from client
+    receivedData = Receiver.rdt_recv()
+    # arrange data back into img and save result
+    img = np.asarray(bytearray(receivedData), dtype=np.uint8)
+    img = cv.imdecode(img, cv.IMREAD_COLOR)
+    cv.imwrite("result.bmp", img)
+    # open result image!
+    print("Opening image: ")
+    cv.imshow('result.bmp', img)
+    cv.waitKey()
---
+++ Phase3/UDPSender.py
@@ -0,0 +1,22 @@
+# import entire socket & rdt library for use
+from socket import *
+from RDT2_protocol import RDT2
+
+# initialize server info
+sndrName = '127.0.0.1'
+sndrPort = 10420
+
+# create sender primed for reliable data transport and bind to local port
+Sender = RDT2(sndrName, sndrPort)
+Sender.UDPsocket.bind(('', sndrPort))
+
+# loop client to allow for multiple file sends
+while True:
+    # prompt input and send message to socket
+    filePath = input('Input path of image file: ')
+    img = open(filePath, 'rb')  # open img
+    imgData = img.read() # read data of img
+    sendAddr = (sndrName, 10421) # assign receiver
+    print('Image sending...')
+    Sender.rdt_send(imgData, sendAddr)
+    print('Image sent!')
